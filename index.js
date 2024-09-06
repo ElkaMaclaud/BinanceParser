ff13f5970f0102f8ff91b3704601bd2b65c6aaa1
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-09-06 21:43:15 +0300

    requires some improvement

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra"
+import Plagin from "puppeteer-extra-plugin-stealth";
 import fs from "fs";
 
 (async () => {
+  puppeteer.use(Plagin())
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+  let ru;
+  await page.goto(
+    "https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D0%B0+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&oq=rehc+ljkkfhf&gs_lcrp=EgZjaHJvbWUqDwgCEAAYChiDARixAxiABDIGCAAQRRg5MhIIARAAGAoYgwEYsQMYyQMYgAQyDwgCEAAYChiDARixAxiABDISCAMQABgKGIMBGJIDGLEDGIAEMg8IBBAAGAoYgwEYsQMYgAQyDwgFEAAYChiDARixAxiABDIJCAYQABgKGIAEMgwIBxAAGAoYsQMYgAQyDAgIEAAYChixAxiABDIJCAkQABgKGIAE0gEINTM5OWowajeoAgCwAgA&sourceid=chrome&ie=UTF-8",
+    {
+      waitUntil: "domcontentloaded",
+    }
+  );
+   await page.goto(
+    "https://finance.rambler.ru/currencies/USD/",
+    {
+      waitUntil: "domcontentloaded",
+    }
+  );
+  await page.evaluate(() => {
+    window.scrollBy(0, 300);
+  });
+  await new Promise((resolve) => {
+    setTimeout(resolve, 5000);
+  });
+  await page.waitForSelector('._ZXx92_y', {visible: true})
+  await page.evaluate(() => {
+    ru = document.querySelector('._ZXx92_y').innerText.trim()
+    return ru
+  })
+  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!", ru)
+
+
+  // Идём в binance
   await page.goto("https://www.binance.com/ru/markets/overview?p=1", {
     waitUntil: "domcontentloaded",
   });
@@ -30,7 +60,8 @@ import fs from "fs";
         document.querySelectorAll('div[direction="ltr"]'),
         (el) => ({
           name: el.querySelector(".tab__column").innerText.replace("\n", " | ").padEnd(45, ' '),
-          price: el.querySelector('div[data-area="right"').innerText,
+          price: el.querySelector('div[data-area="right"').innerText.padEnd(18, ' '),
+          priceRu: (parseFloat(el.querySelector('div[data-area="right"').innerText.trim())) * 90.6
         })
       );
       return list
@@ -46,7 +77,7 @@ import fs from "fs";
     arrCurrency.push(...arr)
   }
 
-  fs.writeFileSync("data.txt", arrCurrency.map(el=>`${el.name}: ${el.price}`).join("\n"))
+  fs.writeFileSync("data.txt", arrCurrency.map(el=>`${el.name}: ${el.price} | в рублях: ${el.priceRu}`).join("\n"))
   console.log("КОНЕЦ!!!")
   
   await browser.close()
